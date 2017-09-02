3cd53347a4f5082fbcb9bcb6639fda330486b0ed
fuzzybabybunny <victor@victorlinphoto.com>
2017-09-02 02:49:14 -0400

    Used shortid npm package to generate shortIds and have app redirect to url

--- server/main.js
+++ server/main.js
@@ -7,9 +7,11 @@ import '/imports/startup/simple-schema-configuration';
 Meteor.startup(() => {
   // code to run on server at startup
   WebApp.connectHandlers.use( (req, res, next) => {
+  	console.log(req.url);
   	const _id = req.url.slice(1);
   	// console.log('req: ', req.url, req.method, req.headers);
   	const link = Links.findOne({_id});
+  	console.log(link);
   	if(link){
 	  	res.statusCode = 302;
 	  	res.setHeader('Location', link.url);
